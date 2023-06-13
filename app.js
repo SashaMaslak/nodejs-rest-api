b892dbe4e0487c58d2229727e2d876ca161c441f
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-06-13 09:49:14 +0300

    fixed: app

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const express = require("express")
 const logger = require("morgan")
 const cors = require("cors")
 
+// ROUTER FOR CONTACTS ===============================
 const contactsRouter = require("./routes/api/contacts")
+const authRouter = require("./routes/api/auth")
 
+// SERVER EXPRESS ===============================
 const app = express()
 
+// LOGGER FOR SERVER ===============================
 const formatsLogger = app.get("env") === "development" ? "dev" : "short"
 
+// MIDDLEWARES FOR SERVER ===============================
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+// ENDPOINTS ==============================
 app.use("/api/contacts", contactsRouter)
+app.use("/api/auth", authRouter)
 
-app.use((req, res) => {
+// UNKNOWN REQUEST HANDLER =================
+app.all("*", (req, res) => {
 	res.status(404).json({ message: "Not found" })
 })
 
+// GLOBAL ERROR HANDLER =====================
 app.use((err, req, res, next) => {
 	const { status = 500, message = "Server error" } = err
 	res.status(status).json({ message })
