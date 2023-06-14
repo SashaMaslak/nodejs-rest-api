75f4aeadf54cfed6f660ce1baeb94713ea852f72
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-06-14 14:34:50 +0300

    test: prettier

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,83 +7,82 @@ const { ctrlWrapper, HttpError } = require("../helpers/index.js")
 
 const { SECRET_KEY } = process.env
 
-// SIGN UP CONTROLLER =====================================
 const register = async (req, res) => {
-	const { email, password } = req.body
-	const user = await User.findOne({ email })
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
 
-	if (user) throw HttpError(409, "Email already in use")
+  if (user) throw HttpError(409, "Email already in use")
 
-	const hashPassword = await bcrypt.hash(password, 10)
+  const hashPassword = await bcrypt.hash(password, 10)
 
-	const newUser = await User.create({ ...req.body, password: hashPassword })
-	res.status(201).json({
-		user: {
-			name: newUser.name,
-			email: newUser.email,
-			subscription: newUser.subscription,
-		},
-	})
+  const newUser = await User.create({ ...req.body, password: hashPassword })
+  res.status(201).json({
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  })
 }
 
-// SIGN IN CONTROLLER =======================
+// SIGN IN CONTROLLER // =======================
 const login = async (req, res) => {
-	const { email, password } = req.body
-	const user = await User.findOne({ email })
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
 
-	if (!user) throw HttpError(401, "Email or password is wrong")
+  if (!user) throw HttpError(401, "Email or password is wrong")
 
-	const passwordCompare = await bcrypt.compare(password, user.password)
-	if (!passwordCompare) throw HttpError(401, "Email or password is wrong")
+  const passwordCompare = await bcrypt.compare(password, user.password)
+  if (!passwordCompare) throw HttpError(401, "Email or password is wrong")
 
-	const payload = {
-		id: user._id,
-	}
+  const payload = {
+    id: user._id,
+  }
 
-	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" })
-	await User.findByIdAndUpdate(user._id, { token })
-	res.json({
-		token,
-		user: {
-			name: user.name,
-			email: user.email,
-			subscription: user.subscription,
-		},
-	})
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" })
+  await User.findByIdAndUpdate(user._id, { token })
+  res.json({
+    token,
+    user: {
+      name: user.name,
+      email: user.email,
+      subscription: user.subscription,
+    },
+  })
 }
 
 // LOG OUT CONTROLLER =====================================
 const logout = async (req, res) => {
-	const { _id } = req.user
-	await User.findByIdAndUpdate(_id, { token: "" })
-	res.status(204, "Logout success").json({
-		message: "Logout success",
-	})
+  const { _id } = req.user
+  await User.findByIdAndUpdate(_id, { token: "" })
+  res.status(204, "Logout success").json({
+    message: "Logout success",
+  })
 }
 
 // GET CURRENT USER CONTROLLER ============================
 const getCurrent = async (req, res) => {
-	const { email, name } = req.user
-	res.json({ email, name })
+  const { email, name } = req.user
+  res.json({ email, name })
 }
 
 // UPDATE SUBSCRIPTION CONTROLLER ============================
 const updateSubscription = async (req, res) => {
-	// const { subscription } = req.body
-	// if (subscription !== "starter") throw HttpError(400)
+  // const { subscription } = req.body
+  // if (subscription !== "starter") throw HttpError(400)
 
-	const { _id } = req.user
-	const result = await User.findByIdAndUpdate(_id, req.body, {
-		new: true,
-	})
-	if (!result) throw HttpError(404, "Not found")
-	res.json(result)
+  const { _id } = req.user
+  const result = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  })
+  if (!result) throw HttpError(404, "Not found")
+  res.json(result)
 }
 
 module.exports = {
-	register: ctrlWrapper(register),
-	login: ctrlWrapper(login),
-	logout: ctrlWrapper(logout),
-	getCurrent: ctrlWrapper(getCurrent),
-	updateSubscription: ctrlWrapper(updateSubscription),
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  logout: ctrlWrapper(logout),
+  getCurrent: ctrlWrapper(getCurrent),
+  updateSubscription: ctrlWrapper(updateSubscription),
 }
