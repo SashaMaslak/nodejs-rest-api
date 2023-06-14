3e072368c153077e1a18336cfffee250d5925dc2
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-06-14 16:56:28 +0300

    up to first part

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody")
 const isValidId = require("./isValidId")
 const authenticate = require("./authenticate")
+const uploadFile = require("./uploadFile")
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
+  uploadFile,
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const ctrl = require("../../controllers/auth")
-const { validateBody, authenticate } = require("../../middlewares")
+const { validateBody, authenticate, uploadFile } = require("../../middlewares")
 const { schemas } = require("../../models/user")
 
 const router = express.Router()
@@ -18,6 +18,13 @@ router.patch(
   authenticate,
   validateBody(schemas.updateSubSchema),
   ctrl.updateSubscription
+)
+
+router.patch(
+  "/avatars",
+  uploadFile.single("avatarURL"),
+  authenticate,
+  ctrl.updateAvatar
 )
 
 module.exports = router
