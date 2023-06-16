9bfa291b574986da446719067b8c94bbb7fbdbd3
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-06-16 10:40:11 +0300

    add: test register

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,7 +1,7 @@
 const supertest = require("supertest")
 const app = require("../../app")
 const mongoose = require("mongoose")
-const User = require("../../models/user")
+const { User } = require("../../models/user")
 
 require("dotenv").config()
 const { DB_HOST_TEST } = process.env
@@ -12,12 +12,34 @@ describe("POST /login", () => {
   })
 
   afterAll(async () => {
+    await User.deleteMany()
     await mongoose.disconnect()
+  })
+
+  // beforeEach(async () => {
+  //   await User.deleteMany()
+  // })
+
+  it("should register user", async () => {
+    const response = await supertest(app).post("/users/register").send({
+      name: "testUser2",
+      email: "testUser2@gmail.com",
+      password: "123456",
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toStrictEqual({
+      user: {
+        email: expect.any(String),
+        name: expect.any(String),
+        subscription: expect.any(String),
+      },
+    })
   })
 
   it("should login user", async () => {
     const response = await supertest(app).post("/users/login").send({
-      email: "testUser1@gmail.com",
+      email: "testUser2@gmail.com",
       password: "123456",
     })
 
